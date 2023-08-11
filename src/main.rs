994de12ba16580a2bf52bb29d7c8adec9cf65ae8
bitcapybara <yuxing.bitcapybara@gmail.com>
2023-08-11 20:38:57 +0800

    app: setup first case

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
+use axum::{routing::post, Router};
+use clap::Parser;
+
 pub mod types;
 
-fn main() {
-    println!("hello, world")
+#[derive(clap::Parser)]
+struct Args {
+    #[arg(short, long, default_value = "0.0.0.0", env = "TG_CUCKOO_BOT_IP")]
+    ip: String,
+    #[arg(short, long, default_value_t = 9077, env = "TG_CUCKOO_BOT_PORT")]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+    let app = Router::new().route(
+        "/",
+        post(|| async {
+            println!("====");
+            "hello, world"
+        }),
+    );
+
+    let addr = format!("{}:{}", args.ip, args.port).parse().unwrap();
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
